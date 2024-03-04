d1c989a5de64130f0dfb37a11529f296400ff8a2
Peter Bresic <19489992+Peet-HD@users.noreply.github.com>
2024-03-04 01:51:48 +0100

    Made Gitlab Container usable

---
+++ Models/Gitlab/PersonalAccessToken.cs
@@ -0,0 +1,27 @@
+﻿namespace TestContainer.Shared.Models.Gitlab;
+
+public record PersonalAccessToken
+{
+    public string Name { get; set; } = string.Empty;
+    public string User { get; set; } = string.Empty;
+    public PersonalAccessTokenScopes Scope { get; set; } = PersonalAccessTokenScopes.None;
+    public int ExpirationInDays { get; set; } = 365;
+    internal string TokenInternal { get; set; } = string.Empty;
+    public string Token => TokenInternal;
+}
+
+[Flags]
+public enum PersonalAccessTokenScopes
+{
+    None = 0,
+    api = 2,
+    read_api = 4,
+    read_user = 8,
+    read_repository = 16,
+    write_repository = 32,
+    read_registry = 64,
+    write_registry = 128,
+    create_runner = 256,
+    ai_features = 512,
+    k8s_proxy = 1024,
+}
---
+++ RegexPatterns/GitlabTokens.cs
@@ -0,0 +1,58 @@
+﻿using System.ComponentModel;
+using System.Text.RegularExpressions;
+
+namespace TestContainer.Shared.Containers.RegexPatterns;
+public static partial class GitlabTokens
+{
+    [Description("GitLab Personal Access Token")]
+    [GeneratedRegex(@"glpat-[0-9a-zA-Z_\-]{20}")]
+    private static partial Regex gitlabPersonalAccessToken();
+    public static Regex GitlabPersonalAccessToken => gitlabPersonalAccessToken();
+
+    [Description("GitLab Pipeline Trigger Token")]
+    [GeneratedRegex(@"glptt-[0-9a-zA-Z_\-]{40}")]
+    private static partial Regex gitlabPipelineTriggerToken();
+    public static Regex GitlabPipelineTriggerToken => gitlabPipelineTriggerToken();
+    [Description("GitLab Runner Registration Token")]
+    [GeneratedRegex(@"GR1348941[0-9a-zA-Z_\-]{20}")]
+    private static partial Regex gitlabRunnerRegistrationToken(); 
+    public static Regex GitlabRunnerRegistrationToken => gitlabRunnerRegistrationToken();
+    [Description("GitLab Runner Authentication Token")]
+    [GeneratedRegex(@"glrt-[0-9a-zA-Z_\-]{20}")]
+    private static partial Regex gitlabRunnerAuthenticationToken();
+    public static Regex GitlabRunnerAuthenticationToken => gitlabRunnerAuthenticationToken();
+    [Description("GitLab Feed Token")]
+    [GeneratedRegex(@"feed_token=[0-9a-zA-Z_\-]{20}")]
+    private static partial Regex gitlabFeedToken();
+    public static Regex GitlabFeedToken => gitlabFeedToken();
+
+    [Description("GitLab OAuth Application Secrets")]
+    [GeneratedRegex(@"gloas-[0-9a-zA-Z_\-]{64}")]
+    private static partial Regex gitlabOauthApplicationToken();
+    public static Regex GitlabOauthApplicationToken => gitlabOauthApplicationToken();
+
+    [Description("GitLab Feed token v2")]
+    [GeneratedRegex(@"glft-[0-9a-zA-Z_\-]{20}")]
+    private static partial Regex gitlabFeedTokenV2();
+    public static Regex GitlabFeedTokenV2 => gitlabFeedTokenV2();
+    [Description("GitLab Agent for Kubernetes token")]
+    [GeneratedRegex(@"glagent-[0-9a-zA-Z_\-]{50}")]
+    private static partial Regex gitlabK8sAgentToken();
+    public static Regex GitlabK8sAgentToken => gitlabK8sAgentToken();
+    [Description("GitLab Incoming email token")]
+    [GeneratedRegex(@"glimt-[0-9a-zA-Z_\-]{25}")]
+    private static partial Regex gitlabIncomingEmailToken();
+    public static Regex GitlabIncomingEmailToken => gitlabIncomingEmailToken();
+    [Description("GitLab Deploy Token")]
+    [GeneratedRegex(@"gldt-[0-9a-zA-Z_\-]{20}")]
+    private static partial Regex gitlabDeployToken();
+    public static Regex GitlabDeployToken => gitlabDeployToken();
+    [Description("GitLab SCIM token")]
+    [GeneratedRegex(@"glsoat-[0-9a-zA-Z_\-]{20}")]
+    private static partial Regex gitlabScimToken();
+    public static Regex GitlabScimToken => gitlabScimToken();
+    [Description("GitLab CI Build (Job) token")]
+    [GeneratedRegex(@"glcbt-[0-9a-zA-Z]{1,5}_[0-9a-zA-Z_-]{20}")]
+    private static partial Regex gitlabCiBuildJobToken();
+    public static Regex GitlabCiBuildJobToken => gitlabCiBuildJobToken();
+}
